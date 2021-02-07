c539b61d6954cab74095bbaff11b5d46fff27a30
nalumedia <ryan.dranginis@gmail.com>
2021-02-07 09:20:43 -0800

    updated bucket config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
   s3_host_name: "s3-us-west-2.amazonaws.com",
   s3_region: 'us-west-2', # or ENV['AWS_REGION']
   s3_credentials: {
-    bucket: ENV['S3_BUCKET_NAME'],
+    bucket: ENV['AWS_BUCKET'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
   }
