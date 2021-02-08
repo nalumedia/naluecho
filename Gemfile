697d8dee2ccb4467c83a85705e7f066e9e32a6d8
nalumedia <ryan.dranginis@gmail.com>
2021-02-08 06:52:52 -0800

    added devise

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ ruby '3.0.0'
 gem 'rails', '~> 6.1.1'
 # Use sqlite3 as the database for Active Record
 
+gem 'devise'
+gem 'bootstrap-sass'
 gem 'kt-paperclip'
 gem 'aws-sdk'
 gem 'pg'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_151347) do
+ActiveRecord::Schema.define(version: 2021_02_08_144412) do
 
   create_table "rigs", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 2021_02_07_151347) do
     t.string "image_content_type"
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
